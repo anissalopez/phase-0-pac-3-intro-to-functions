33cd5d17929167ead2f3792910bee19982450072
AnissaLopez <anissa965@gmail.com>
2023-03-21 15:03:18 -0400

    parameters

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Follow along with the examples here
+//function doNothing() {}
+/*function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello()
+
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}*/
+/*function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+sayHelloToGuadalupe()*/
+/*function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("anything")*/
+  /*function add(x, y) {
+    return x + y;
+  }
+  add (1, 2)
+  console.log(add(1, 2))*/
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+  say(hi, Anissa)
